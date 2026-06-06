13999c5d4185d7853489372467485189a7d0ef43
saatwikdas69420 <127622420+saatwikdas69420@users.noreply.github.com>
2026-06-06 13:41:21 -0400

    Implement horizontal scrolling effect on sections

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+const sections = document.querySelectorAll(".horizontal-section");
 
+function updateHorizontalScroll() {
+sections.forEach(section => {
+const track = section.querySelector(".horizontal-track");
+
+const rect = section.getBoundingClientRect();
+
+const scrollableHeight =
+  section.offsetHeight - window.innerHeight;
+
+let progress =
+  Math.max(
+    0,
+    Math.min(
+      1,
+      -rect.top / scrollableHeight
+    )
+  );
+
+const maxTranslate =
+  track.scrollWidth - window.innerWidth + 120;
+
+track.style.transform =
+  `translateX(${-progress * maxTranslate}px)`;
+
+});
+}
+
+window.addEventListener("scroll", updateHorizontalScroll);
+window.addEventListener("resize", updateHorizontalScroll);
+
+updateHorizontalScroll();
